77fe32ee0fd6d6e4ee82191f01e9045143812f45
yjwmml <yjwmml@gmail.com>
2015-06-11 22:47:36 +0800

    Initial Commit

---
+++ DragGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragGame
+//
+//  Created by HEWei on 15/6/11.
+//  Copyright (c) 2015年 HEWei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
