0ac1cd3ec9ca7ecce25336a659eacf2f6389cb89
yjwmml <yjwmml@gmail.com>
2015-06-12 23:21:34 +0800

    我的第一个ios程序
    我的第一个IOS程序

---
+++ DragGame/InfoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InfoViewController.swift
+//  DragGame
+//
+//  Created by HEWei on 15/6/12.
+//  Copyright (c) 2015年 HEWei. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func onBack(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- DragGame/ViewController.swift
+++ DragGame/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderTarget: UISlider!
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var labScore: UILabel!
+    @IBOutlet weak var labRound: UILabel!
+    
+    var targetValue :Int = 0
+    var currValue :Int = 0
+    var totalScore :Int = 0
+    var totalRound :Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        onUpate();
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderChange(sender: AnyObject) {
+        currValue = lroundf(mySlider.value)
+    }
+
+    @IBAction func onAlert(sender: AnyObject) {
+        var different = currValue - targetValue
+        if (different < 0) {
+            different *= -1
+        }
+        var points = 100 - different
+        totalScore += points
+        var message :String
+        switch (points) {
+        case 100:
+            message = "太牛了，大侠的得分是：\(points)"
+        case 90..<100:
+            message = "大侠的得分是：\(points)"
+        case 80...90:
+            message = "大侠您的眼力还差点，得分是：\(points)"
+        default:
+            message = "您的眼里差的有点离谱了，只有\(points)分"
+        }
+        let alert = UIAlertView(title: "老镇帅不帅", message: message, delegate: self, cancelButtonTitle: "帅")
+        alert.show()
+    }
+    
+    func alertView(alertView: UIAlertView!, didDismissWithButtonIndex buttonIndex: Int) {
+        onUpate()
+    }
+    
+    @IBAction func reSet(sender: AnyObject) {
+        currValue = 50
+        totalRound = 0
+        totalScore = 0
+        onUpate()
+    }
+    
+    @IBAction func onShowInfo(sender: AnyObject) {
+        let controller = InfoViewController(nibName: "InfoViewController", bundle:nil)
+        controller.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func onUpate() {
+        targetValue = Int(1 + arc4random() % 100)
+        sliderTarget.value = Float(targetValue)
+        labScore.text = String(self.totalScore)
+        totalRound++;
+        labRound.text = String(self.totalRound)
+        mySlider.value = 50
+    }
+    
+    
 
 }
 
